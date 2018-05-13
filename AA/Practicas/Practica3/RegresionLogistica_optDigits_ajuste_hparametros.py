07a133281d2439a49d74ccea510bcd7c7e19ea09
Antonio Manuel Fresneda Rodríguez <antoniomanuelfr@gmail.com>
2018-05-13 20:54:28 +0200

    Probando cosas

--- AA/Practicas/Practica3/RegresionLogistica_optDigits_ajuste_hparametros.py
+++ AA/Practicas/Practica3/RegresionLogistica_optDigits_ajuste_hparametros.py
@@ -9,7 +9,10 @@ from sklearn.metrics import classification_report
 from sklearn.pipeline import Pipeline
 from sklearn.metrics import confusion_matrix
 from sklearn.model_selection import train_test_split
+from sklearn.externals import 	joblib
 import numpy as np
+import itertools
+import matplotlib.pyplot as plt
 
 seed = 1997
 
@@ -58,3 +61,52 @@ for score in scores:
               % (mean, std * 2, params))
 	
 	print("\n\n\n END OF TUNNING PARAMETERS!!!\n\n\n")
+	X = np.load("datos/optdigits_tra_X.npy")
+y =np.load("datos/optdigits_tra_y.npy")
+X_true_test=np.load("datos/optdigits_tes_X.npy")
+y_true_test=np.load("datos/optdigits_tes_y.npy")
+class_names=range(0,y.shape[0])
+pipe=Pipeline([('Scale',preprocessing.StandardScaler()),
+			   ('Norm',preprocessing.Normalizer())])
+pipe.fit(X)
+X=pipe.transform(X)
+
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+
+print ("The model is trained on the full train set and with best parameters")
+best_logistic_model=LogisticRegression(**clf.best_params_,fit_intercept=False)
+best_logistic_model.fit(X,y)
+print("The model is going to be saved in: LogicRegresion_model.pkl")
+joblib.dump(best_logistic_model,'LogicRegresion_model.pkl')
